32597b43dda2b2c369eee68102963f581c80857c
parseval <rainbekov99@gmail.com>
2021-03-05 15:04:35 +0300

    i add tcp sniffer

--- sniffer.py
+++ sniffer.py
@@ -1,7 +1,23 @@
 import pyshark
 
+
 if __name__ == '__main__':
     capture = pyshark.LiveCapture(interface='Ethernet 3', bpf_filter='tcp port 443')
-    capture.sniff(timeout=10, packet_count=5)
+    capture.sniff(timeout=5, packet_count=3)
     for packet in capture:
-        print(packet)
+        print('Destination:', packet.ip.dst)
+        print('Ack:', packet.tcp.flags_ack)
+        print('Syn:', packet.tcp.flags_syn)
+        print('Seg:', packet.tcp.seq)
+        print('Source:', packet.ip.src)
+        print("***********************")
+
+    capture_proxy = pyshark.LiveCapture(interface='Ethernet 3', bpf_filter='tcp port 3128')
+    capture_proxy.sniff(timeout=5, packet_count=3)
+    for packet in capture_proxy:
+        print('Destination:', packet.ip.dst)
+        print('Ack:', packet.tcp.flags_ack)
+        print('Syn:', packet.tcp.flags_syn)
+        print('Seq:', packet.tcp.seq)
+        print('Source:', packet.ip.src)
+        print("***********************")
